a9d92cd16ed8b596a03bd51cac6a842ccc324965
Paul O'Shea <poshea48@msn.com>
2020-05-18 16:17:13 -1000

    commented out siteUrl, fixed 404 doesn't

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import SEO from '../components/layout/SEO';
 
 const NotFoundPage = () => (
   <>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>Sorry this page doesn&lsquot exist</p>
+    <p>Sorry this page {'"doesn\'t"'} exist</p>
+    <Link to="/">Back Home</Link>
   </>
 );
 
